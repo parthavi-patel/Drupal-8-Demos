7597d9aca6eec62e10bd0ff4835e74d97163da6d
parthavi-patel <parthavi.neosoft@gmail.com>
2020-03-06 18:59:09 +0530

    Create event for config, implemets callback fuction for same,when update/save congig data display custom message

---
+++ hookevent/src/ExampleEvent.php
@@ -0,0 +1,23 @@
+<?php 
+namespace Drupal\hookevent;
+use Symfony\Component\EventDispatcher\Event;
+
+class ExampleEvent extends Event {
+
+  const SUBMIT = 'event.submit';
+  protected $referenceID;
+
+  public function __construct($referenceID)
+  {
+    $this->referenceID = $referenceID;
+  }
+
+  public function getReferenceID()
+  {
+    return $this->referenceID;
+  }
+
+  public function myEventDescription() {
+    return "This is as an example event";
+  }
+}
---
+++ hookevent/src/Form/DemoEventDispatchForm.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\hookevent\Form\DemoEventDispatchForm.
+ */
+
+namespace Drupal\hookevent\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\hookevent\ExampleEvent;
+
+/**
+ * Class DemoEventDispatchForm.
+ *
+ * @package Drupal\hookevent\Form
+ */
+class DemoEventDispatchForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'demo_event_dispatch_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['name'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('Reference'),
+      '#description' => $this->t('Type something here that will be set to the event object, while subscribing it.'),
+      '#maxlength' => 64,
+      '#size' => 64,
+    );
+    $form['dispatch'] = array(
+      '#type' => 'submit',
+      '#value' => $this->t('Dispatch'),
+    );
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // Following is the example for
+    // How to dispatch an event in Drupal 8?
+    $dispatcher = \Drupal::service('event_dispatcher');
+    $event = new ExampleEvent($form_state->getValue('name'));
+    $dispatcher->dispatch(ExampleEvent::SUBMIT, $event);
+    //echo "<pre>"; print_r($dispatcher); die;
+  }
+}
